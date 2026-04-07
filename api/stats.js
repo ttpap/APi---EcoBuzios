092dd8cdbdbe57a55729867be0e6a436a30caaed
Pap <pap@Paps-MacBook-Air.local>
2026-04-07 10:06:28 -0300

    feat: dashboard EcoBúzios com proxy Vercel

---
+++ api/stats.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  const response = await fetch(
+    'https://ixgujnhdjrgoakqzdkgx.supabase.co/functions/v1/public-stats-api',
+    { headers: { 'x-api-key': 'c04248b422b59e718e8115a66286b1f9a56f5f447b44354128ad7406ebb50752' } }
+  );
+  const data = await response.json();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json(data);
+}
